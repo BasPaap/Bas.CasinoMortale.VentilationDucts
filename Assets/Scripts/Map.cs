d1e94e5517acbed227afc4e1b89a014348da0bbd
Bas Paap <paap@kissthefrog.nl>
2021-10-15 17:23:08 +0200

    Document all methods in Map.

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -19,6 +19,9 @@ public class Map : MonoBehaviour
         Load();
     }
 
+    /// <summary>
+    /// Loads map data from a file.
+    /// </summary>
     internal void Load()
     {
         if (!File.Exists(FullPath))
@@ -34,6 +37,10 @@ public class Map : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Saves the map to a file.
+    /// </summary>
+    /// <param name="mapData">The map data to save.</param>
     private void Save(MapData mapData)
     {
         var serializer = new XmlSerializer(typeof(MapData));
@@ -44,6 +51,10 @@ public class Map : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Returns a default 10x10 map.
+    /// </summary>
+    /// <returns>A default 10x10 map.</returns>
     private MapData CreateDefaultMap()
     {
         // Create an empty 10x10 map
@@ -56,6 +67,9 @@ public class Map : MonoBehaviour
         return defaultMapData;
     }
 
+    /// <summary>
+    /// Creates a backup of the current map file.
+    /// </summary>
     internal void CreateBackup()
     {
         if (File.Exists(FullPath))
