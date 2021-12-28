e195e1efed5ad309bebb374fd5611a84edaf1ef7
Bas Paap <baspaap@live.nl>
2021-12-28 16:22:54 +0100

    Make PlayerMovementController use gravity.

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -18,7 +18,6 @@ public class PlayerMovementController : MonoBehaviour
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        //characterController.attachedRigidbody.useGravity = false;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -31,6 +30,7 @@ public class PlayerMovementController : MonoBehaviour
     private void Update()
     {
         Physics.SyncTransforms(); // This is necessary in case the player's transform has been changed by something other than player input (for instance, changing the map size). See: https://issuetracker.unity3d.com/issues/charactercontroller-overrides-objects-position-when-teleporting-with-transform-dot-position
+        movement.y = characterController.isGrounded ? 0.0f : -9.8f;
         velocity = Vector3.Lerp(velocity, movement, damping * Time.deltaTime);
         velocity = velocity.magnitude < 0.001f ? Vector3.zero : velocity;   // To prevent infinitely small lerping.
         characterController.Move(velocity * Time.deltaTime);
